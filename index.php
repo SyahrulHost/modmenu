be0615445e7b4231e94e7f372ef3831dd4477058
SyahrulHost <syahrulstay74@gmail.com>
2025-01-21 21:57:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <marquee width="70%" behavior="scroll" style="display: block;
+position: fixed;
+bottom: 70 px;
+left: 15 px;
+z-index: 1000;
+cursor: pointer;
+width: 100%;"><font color="white" style="text-shadow: 0 0 0.2em #ff0000, 0 0 0.2em #ff0000,  0 0 0.2em #ff0000"><b></b></font>MOD MENU IOS INI DIBUAT OLEH @SYAHRUL</marquee>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syahrul ModMenu IOS</title>
+    <style>
+        body {
+            background-color: #000; /* Nền màu đen */
+            color: #fff; /* Màu chữ trắng */
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 50px;
+        }
+        button {
+            background-color: #ff0000; /* Nền màu đỏ */
+            color: #fff; /* Màu chữ trắng */
+            border: none;
+            padding: 15px 30px;
+            font-size: 16px;
+            cursor: pointer;
+            margin: 10px;
+            border-radius: 5px;
+        }
+        button:hover {
+            background-color: #00ff37; /* Đổi màu khi hover */
+        } 
+    </style>
+</head>
+<body>
+    <h1> Syahrul ModMenu IOS </h1>
+    <button onclick="fs1()">Team Speed</button>
+    <button onclick="fs2()">Bật Speed</button>
+    <button onclick="fs3()">Xoá Speed</button>
+    <button onclick="fs4()">Mod Sói</button>
+    <button onclick="fs5()">Mod Naruto</button>
+    <button onclick="fs6()">RS ke Default</button>
+    <button onclick="fs7()">Mod HĐ Naruto</button>
+    <button onclick="fs8()">Speed Test fs8</button>
+    <button onclick="fs9()">Speed Test fs9</button>
+    <script>
+     //Mã 1 : 4366458311853786803
+     //Mã 2 : 4366458311853685297
+     //Mã 3 : 4366458311853765201
+     function fs1() {
+            h5gg.clearResults();
+            h5gg.searchNumber('4397530849764387586', 'I64', '0x100000000', '0x200000000');
+        }
+
+        function fs3() {
+            h5gg.editAll('4397530849764387586', 'I64');
+        }
+
+        function fs2() {
+            h5gg.editAll('4366458311853765201', 'I64');
+        }
+        function fs4() {
+        h5gg.clearResults();
+        h5gg.searchNumber('914000001', 'I32', '0x100000000', '0x200000000');
+        h5gg.editAll('914000003', 'I32');
+        } 
+        function fs5() {
+        h5gg.clearResults();
+        h5gg.searchNumber('914000003', 'I32', '0x100000000', '0x200000000');
+        h5gg.editAll('914047001', 'I32');
+        } 
+        function fs6() {
+        h5gg.clearResults();
+        h5gg.searchNumber('914047001', 'I32', '0x100000000', '0x200000000');
+        h5gg.editAll('914000001', 'I32');
+        } 
+        function fs7() {
+        h5gg.clearResults();
+        h5gg.searchNumber('914047001', 'I32', '0x100000000', '0x200000000');
+        h5gg.editAll('912047001', 'I32');
+        } 
+        function fs8() {
+            h5gg.editAll('4366458311853786803', 'I64');
+        }
+        function fs9() {
+            h5gg.editAll('4366458311853685297', 'I64');
+        }
+    </script>
+</body>
+</html>
